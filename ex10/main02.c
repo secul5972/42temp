3019a03dc902f1432ee044c659d1abb77f190e16
secul5972 <secul5972@naver.com>
2021-06-21 23:46:28 +0900

    r

--- ex10/main02.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-unsigned int	ft_strlcpy(char *dest,char *stc, unsigned int n);
-int main()
-{
-	char a[]="asjdklasj";
-	char b[]="2389048290342";
-	printf("%d %s",ft_strlcpy(a,b,8),a);
-}
